b26aad7f16db339a6ecace8c6ec1ccd9052099b6
LAGHZALI taha <mrtahalz@gmail.com>
2020-12-18 17:11:44 +0100

    renamne functions and adding comments

--- arrowFunction.js
+++ arrowFunction.js
@@ -1,8 +1,9 @@
-// arrow function
+// arrow function syntax
 const sum = (x,y) => x+y
 const oneArgument = x => x
 const multipleInstructions = () => {
     console.log('a');
     console.log('a');
     console.log('a');
+    return 'a'
 }
